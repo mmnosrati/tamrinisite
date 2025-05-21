0da51c174487479f3416a14a705447c59b096602
mmnosrati <mmnosrati78@gmail.com>
2025-05-22 00:33:17 +0330

    add test view functionality

--- firstapp/views.py
+++ firstapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def test_view(request):
+    return HttpResponse("<h1>hello, this is test page</h1>")
